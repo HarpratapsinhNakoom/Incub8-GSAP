485bdff1516eb04207fef62e9659aed3e14f17db
Harpratapsinh Nakoom <91936753+HarpratapsinhNakoom@users.noreply.github.com>
2023-05-19 15:17:14 +0530

    color change and links integration

--- client/Home Page/js/script.js
+++ client/Home Page/js/script.js
@@ -39,22 +39,22 @@ TweenMax.from(".title", 1, {
 //     ease: Expo.easeInOut
 // })
 
-TweenMax.from(".line-one", 1, {
+// TweenMax.from(".line-one", 1, {
+//     opacity: 0,
+//     delay: 2,
+//     y: "-100vh",
+//     ease: Expo.easeInOut
+// })
+// TweenMax.from(".line-two", 1, {
+//     opacity: 0,
+//     delay: 2.5,
+//     y: "-100vh",
+//     ease: Expo.easeInOut
+// })
+
+TweenMax.from(".img", 1, {
     opacity: 0,
     delay: 2,
-    y: "-100vh",
-    ease: Expo.easeInOut
-})
-TweenMax.from(".line-two", 1, {
-    opacity: 0,
-    delay: 2.5,
-    y: "-100vh",
-    ease: Expo.easeInOut
-})
-
-TweenMax.from(".img", 2, {
-    opacity: 0,
-    delay: 2.9,
     y: "-100vh",
     ease: Expo.easeInOut
 })
