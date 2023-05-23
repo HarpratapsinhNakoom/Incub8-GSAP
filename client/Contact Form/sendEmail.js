7c6364f69975aabd3cdcbdd56605f8e569d55253
Harpratapsinh Nakoom <91936753+HarpratapsinhNakoom@users.noreply.github.com>
2023-05-23 14:06:02 +0530

    contact form completed

---
+++ client/Contact Form/sendEmail.js
@@ -0,0 +1,31 @@
+$('input,textarea').val("");
+  $('.main__form div input, .main__form div textarea').focusout(function() {
+    var text_val = $(this).val();
+    if (text_val === "") {
+      $(this).removeClass('has-value');
+    } else {
+      $(this).addClass('has-value');
+    }
+});
+
+
+const btn = document.getElementById('form_submit_button');
+
+document.getElementById('main__form')
+ .addEventListener('submit', function(event) {
+   event.preventDefault();
+
+   btn.value = 'Sending...';
+
+   const serviceID = 'service_rwv4oyq';
+   const templateID = 'template_ksz847d';
+
+   emailjs.sendForm(serviceID, templateID, this)
+    .then(() => {
+      btn.value = 'Send Message'; 
+      alert('Sent!');
+    }, (err) => {
+      btn.value = 'Send Email';
+      alert(JSON.stringify(err));
+    });
+});
